2ba0618b3070abcb67a79bbabccb263e878a32d1
Noel Yap <noel.yap+github.com@gmail.com>
2026-03-03 13:35:51 -0800

    CHROME_PROFILE env var used

--- git-create-pr.sh
+++ git-create-pr.sh
@@ -4,6 +4,8 @@ shopt -s inherit_errexit
 
 # create pr
 
+export CHROME_PROFILE
+
 git push
 gh pr create --fill --head="$(git branch --show-current)"
 
@@ -12,5 +14,5 @@ readonly PR_URL="$(gh pr view --json url --jq .url)"
 
 "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" \
     --new-window \
-    --profile-directory='Profile 1' \
+    --profile-directory="${CHROME_PROFILE}" \
     "${PR_URL}"
